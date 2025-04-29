4f6415a6fded32d6808125b8909a49667527632f
Arjit Prakher <arjitprakher4@gmail.com>
2025-04-29 21:12:07 +0530

    audio cannot be generated

--- public/javascripts/tts.js
+++ public/javascripts/tts.js
@@ -2,17 +2,47 @@ async function getAllVoices() {
     const voices = document.getElementById('voices');
     const url = "http://localhost:3000/api/voices";
     // const allVoiceNames = [];
-        const response = await fetch(url);
-        const result = await response.json();
-        
-        console.log(result.voices);
+    const response = await fetch(url);
+    const result = await response.json();
 
-        result.voices.forEach(element => {
-            const options = document.createElement('option');
-            options.value = element.name;
-            options.textContent = element.name;
-            voices.appendChild(options);
-        });
-    
+    // console.log(result.voices);
+
+    result.voices.forEach(element => {
+        const options = document.createElement('option');
+        options.value = element.name;
+        options.textContent = element.name;
+        voices.appendChild(options);
+    });
+
 }
 getAllVoices();
+
+const submitbtn = document.getElementById('submitbtn');
+submitbtn.addEventListener('click', async function () {
+
+    const text = document.getElementById('textarea').value;
+    const selectedVoice = document.getElementById('voices').value;
+    submitbtn.innerHTML = "Going..";
+
+    const response = await fetch('/api/tts', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ text, selectedVoice })
+    });
+    console.log(response);
+    if(response.ok) {
+        submitbtn.innerHTML = "Let's go!";
+        const audioBlob = await response.blob();
+        const audioUrl = URL.createObjectURL(audioBlob);
+        const audioPlayer = document.getElementById('audioPlayer');
+        audioPlayer.src = audioUrl;
+        // audio.play();
+    }
+    else {
+        console.error('Error in TTS response: ', response.statusText);
+    }
+
+
+})
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 const { getAllVoices } = require('./getAllVoices');
-// const { getTokenTTS } = require("./authTokenTTS");
+const getTokenTTS = require("./authTokenTTS");
 // const { getTokenSTT } = require("./authTokenSTT");
 var router = express.Router();
 
@@ -16,15 +16,40 @@ router.get('/tts', function (req, res) {
 });
 
 // API endpoint for fetching voices
-router.get('/api/voices', async function(req, res) {
+router.get('/api/voices', async function (req, res) {
   try {
     const voices = await getAllVoices();
     res.send(voices);
-    // res.json(voices);
   } catch (error) {
     console.error('Error fetching voices: ', error);
     res.status(500).send('unable to fetch voices.')
   }
+});
+
+router.post('/api/tts', async function (req, res) {
+  const { text, selectedVoice } = req.body;
+
+
+  const API_URL = 'https://api.us-south.text-to-speech.watson.cloud.ibm.com/instances/6902873c-cd8a-44ed-a634-473967bb5df6/v1/synthesize';
+  const token = await getTokenTTS();
+  try {
+    const response = await fetch(`${API_URL}?voice=${selectedVoice}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`,
+      },
+      body: JSON.stringify({ text })
+    });
+    res.send(response);
+  } catch (error) {
+    console.error('Error in TTS: ', error);
+    res.status(500).send('Error in TTS');
+    
+  }
+
+  
+
 });
 
 
