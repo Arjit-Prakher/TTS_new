917a68f63c187bcf5f2aa943954cc264eb14663b
Arjit Prakher <arjitprakher4@gmail.com>
2025-05-03 00:56:58 +0530

    Fix: displaying textual wait

--- public/javascripts/stt.js
+++ public/javascripts/stt.js
@@ -2,6 +2,7 @@ const submitbtn = document.getElementById("submitbtn");
 const textarea = document.getElementById("textarea");
 
 submitbtn.addEventListener("click", async function() {
+    submitbtn.innerHTML = "Transcribing..";
     const audioFile = document.getElementById('audioFile').files[0];
     if(!audioFile) {
         console.log("No file was selected");
@@ -9,14 +10,15 @@ submitbtn.addEventListener("click", async function() {
     }
     const formData = new FormData();
     formData.append("audioFile", audioFile);
-
+    
     try {
         const response = await fetch("http://localhost:3000/api/stt", {
             method: "POST",
             body: formData
         });
-
+        
         const result = await response.text();
+        submitbtn.innerHTML = "Transcribe";
         textarea.value = result;
     } catch (error) {
         console.log("Error sending the files");
